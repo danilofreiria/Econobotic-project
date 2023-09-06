e6a9344962213ac1a003a8038103520306359e65
Danilo Freiria <76266160+danilofreiria@users.noreply.github.com>
2023-09-06 18:44:53 -0300

    Update: add environment variables

--- src/config/pool.js
+++ src/config/pool.js
@@ -1,12 +1,15 @@
 import pg from 'pg';
 const { Pool } = pg;
+import dotenv from "dotenv";
+dotenv.config();
 
+const key = process.env.poolPassword;
 
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'dindin',
-  password: '123456',
+  password: key,
   port: 5432, 
 });
 
